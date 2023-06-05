6c40cb0783068512f3cb62d75f4f433b01629126
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-05 16:09:43 +0200

    sort works

--- src/pages/PageArticles.ts
+++ src/pages/PageArticles.ts
@@ -1,5 +1,11 @@
 import articles from '../data/articles.json';
 
+// articles.sort((a, b) => 1);
+// articles.sort((a, b) => a.whenPublished < b.whenPublished);
+articles.sort((a, b) => (a.whenPublished < b.whenPublished ? -1 : 1));
+
+console.log(typeof(articles));
+
 export const PageArticles = () => {
 	return /*html*/`
 	<div class="page pageArticles">
@@ -7,6 +13,7 @@ export const PageArticles = () => {
 		${articles.map(article => {
 			return `
 			<div class="article">
+				<div class="supertitle">${article.whenPublished}</div>
 				<div class="title">${article.title}</div>
 				<div class="body">${article.body}</div>
 			</div>
