e29fc26fae977bcd838fa40edfa05910239be0c7
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-05 15:23:45 +0200

    have articles in article page

--- src/pages/PageArticles.ts
+++ src/pages/PageArticles.ts
@@ -1,5 +1,21 @@
+
+const articles = [
+	{
+		title: "New Street in Berlin",
+		body: "There is a new street in Berlin."
+	},
+	{
+		title: "New Street in Hamburg",
+		body: "There is a new street in Hamburg."
+	},
+	{
+		title: "New Street in Paris",
+		body: "There is a new street in Paris."
+	}
+]
+
 export const PageArticles = () => {
 	return /*html*/`
-	<p>These are the articles.</p>
+	<p>These are the ${articles.length} articles.</p>
 `; 
 }
